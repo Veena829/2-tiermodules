7c8dad0f5bb476bfe63f81025e613bdd7861f345
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-04-17 15:35:50 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -24,6 +24,7 @@ variable "public_subnet_cidr_2" {
 
 variable "private_subnet_cidr_1" {
   description = "CIDR block for the first private subnet"
+type = string
   default     = "10.0.3.0/24"
 }
 
