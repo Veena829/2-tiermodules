46c41a28622118bf8333cd487d4f8723daf8c2fa
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-04-17 15:32:48 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,25 +1,30 @@
 variable "aws_region" {
   description = "AWS Region"
+  type = string
   default     = "us-east-1"
 }
 
 variable "s3_bucket_name" {
   description = "S3 bucket name for storing Terraform state"
+ type = string
   default     = "alb-project-bucket123"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
+ type = string
   default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr_1" {
   description = "CIDR block for the first public subnet"
+ type = string
   default     = "10.0.1.0/24"
 }
 
 variable "public_subnet_cidr_2" {
   description = "CIDR block for the second public subnet"
+ type = string
   default     = "10.0.2.0/24"
 }
 
@@ -30,11 +35,13 @@ variable "private_subnet_cidr_1" {
 
 variable "private_subnet_cidr_2" {
   description = "CIDR block for the second private subnet"
+ type = string
   default     = "10.0.4.0/24"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
+ type = string
   default     = "t2.micro"
 }
 
@@ -59,6 +66,7 @@ variable "rds_password" {
 
 variable "ami_id" {
   description = "The AMI ID for the EC2 instances"
+ type = string
   default     = "ami-04aa00acb1165b32a"
 }
 
